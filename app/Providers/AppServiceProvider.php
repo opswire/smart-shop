2ea2f7455b2c58ccf4650cd687fc84b1bd558b09
stanislav <stchikhichin@gmail.com>
2024-01-07 01:30:34 +0300

    feat: AppServiceProvider.php setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,38 @@
 
 namespace App\Providers;
 
+use App\Http\Kernel;
+use Carbon\CarbonInterval;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
     /**
-     * Bootstrap any application services.
+     * @throws BindingResolutionException
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(! app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(! app()->isProduction());
+
+        DB::whenQueryingForLongerThan(CarbonInterval::milliseconds(500), function (Connection $connection) {
+            // todo: Notify development team...
+        });
+
+        /** @var Kernel $kernel */
+        $kernel = app()->make(Kernel::class);
+
+        $kernel->whenRequestLifecycleIsLongerThan(CarbonInterval::seconds(4), function (Connection $connection) {
+            // todo: Notify development team...
+        });
     }
 }
